a4e7e16b3e91b754950f465495180f34a87ec269
shahd03 <shahd.abuzubaida@student.vives.be>
2023-02-22 01:33:30 +0100

    Shahd

--- labo2/Demonstratie Lijsten/js/js.js
+++ labo2/Demonstratie Lijsten/js/js.js
@@ -1,11 +1,9 @@
 const setup = () => {
-    // registeer click event listener bij button
     let btnVoegToe = document.getElementById("btnVoegToe");
     btnVoegToe.addEventListener("click", voegToe);
 }
 
 const voegToe = () => {
-    // Lees de tekst uit het textveld en voeg nieuw <li> element toe
     let txtInput = document.getElementById("txtInput");
     let lstEntries = document.getElementById("lstEntries");
     let tekst = txtInput.value;
